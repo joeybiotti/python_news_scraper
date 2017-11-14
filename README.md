016ded4e3e786824a3087a8d01f0a0e3a4072dc2
Joey Biotti <joey@MacBook-Pro-2.local>
2017-11-13 21:38:10 -0600

    rewrote app.py

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# python news scraper 
+# Python News Scraper 
+
+A python console app that scrapes Google New's RSS Feed. When the code is run, the top headlines on Google News will be displayed in the console. 
+
+Practicing python, especially `import`. This project used `requests` and `BeatifulSoup`. 
--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-import requests
+import bs4
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen
 
-page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
-page
+news_url = "https://news.google.com/news/rss"
+Client = urlopen(news_url)
+xml_page = Client.read()
+Client.close()
 
-print(page)
+soup_page = soup(xml_page, 'xml')
+news_list = soup_page.findAll('item')
+# prints news title, url and pub date
+for news in news_list:
+    print(news.title.text)
+    print(news.link.text)
+    print(news.pubDate.text)
+    print("-"*60)
